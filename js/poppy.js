ca816f204cea0aae24ddd1db9cebff0c00da2e59
Adam Jenkins <adamcole83@gmail.com>
2015-11-10 13:50:12 -0600

    Renamed files and method to Poppy, sound fun, and cleaned up scripts

--- js/poppy.js
+++ js/poppy.js
@@ -1,5 +1,5 @@
 /*!
- * Popout
+ * Poppy
  * A simple jQuery promo popout.
  * http://adamcolejenkins.com
  * @author Adam Jenkins <adamcole83@gmail.com>
@@ -9,9 +9,9 @@
 (function ($, window, document, undefined) {
   'use strict';
 
-  window.Popout = {
+  window.Poppy = {
 
-    name : 'Popout',
+    name : 'Poppy',
 
     version : '1.0.1',
 
@@ -20,12 +20,12 @@
         speed : 200
       },
       klasses : {
-        element : 'popout',
-        open: 'popout-open',
-        control: 'popout-control'
+        element : 'poppy',
+        open: 'poppy-open',
+        control: 'poppy-control'
       },
       cookie : {
-        name: "PopoutCollapse",
+        name: "PoppyCollapse",
         days: 1
       }
     },
@@ -33,6 +33,9 @@
     locked: false,
 
     init : function () {
+
+      // Set our element for Rock & Roll
+      this.$el = $( "." + this.options.klasses.element );
 
       // Check if user has closed the window. When they close, a cookie is
       // set so it doesn't pop out again. The cookie options can be set above.
@@ -46,101 +49,102 @@
 
     },
 
-    openPopout : function (callback) {
-      var $el = $( "." + this.options.klasses.element );
-
-      // Lock the popout so it can't be harmed
-      this.locked = true;
-
-      // Animate the popout open
-      $el.stop().animate({ right: 0 }, this.options.animations.speed, function () {
-
-        // Toggle the open and close classes
-        $el.toggleClass(window.Popout.options.klasses.open);
-
-        // If a callback has been set, we will execute it
-        if ("function" === typeof callback) {
-          callback();
-        }
-
-        // Unlock the popout so we can go on with life
-        window.Popout.locked = false;
-
-      });
-    },
-
-    closePopout : function (callback) {
-      var $el = $( "." + this.options.klasses.element );
-
-      // Lock the popout so it can't be harmed
-      this.locked = true;
-
-      // Find the width of the popout content
-      var position = -($el.outerWidth() - $el.find("." + this.options.klasses.control).outerWidth());
-
-      // Animate the popout close
-      $el.stop().animate({ right: position }, this.options.animations.speed, function () {
-
-        // Toggle the open class
-        $el.toggleClass(window.Popout.options.klasses.open);
-
-        // If a callback has been set, we will execute it
-        if ("function" === typeof callback) {
-          callback();
-        }
-
-        // Unlock the popout so we can go on with life
-        window.Popout.locked = false;
-
-      });
-    },
-
-    isOpen : function () {
-      return $( "." + this.options.klasses.element ).hasClass(this.options.klasses.open) && ! this.locked;
-    },
-
     bindEvents : function () {
-      var $el = $( "." + this.options.klasses.element );
-      var $control = $el.find("." + this.options.klasses.control);
+      var $control = this.$el.find("." + this.options.klasses.control);
 
-      $control.on("click", function () {
-
-        // If the popout has the open class, let's close it
-        if (window.Popout.isOpen()) {
-
-          // Close the popout window
-          window.Popout.closePopout(function () {
-
-            // If the user triggers the popout to close, they're not interested
-            // so let's set the cookie if it hasn't already been set
-            if ( ! window.Popout.cookieIsSet()) {
-              window.Popout.setCookie();
-            }
-
-          });
-        } else {
-
-          // Open the popout window
-          window.Popout.openPopout(function () {
-
-            // If the user triggers the popout to open, clearly they're interested
-            // so we will unset the cookie if it has been set
-            if (window.Popout.cookieIsSet()) {
-              window.Popout.unsetCookie();
-            }
-
-          });
-        }
-      });
+      // On control handle click, toggle the state of which Poppy is in
+      // If it's open, close it & vice versa.
+      $control.on("click", this.toggleState);
 
       // Close the window when the user scrolls down
       $(window).scroll(function () {
 
         // If the popout is open and the user scrolls, close it
-        if(window.Popout.isOpen() &&  $(window).scrollTop() > 100) {
-          window.Popout.closePopout();
+        if(window.Poppy.isOpen() &&  $(window).scrollTop() > 100) {
+          window.Poppy.closePopout();
         }
+
       });
+
+    },
+
+    openPopout : function (callback) {
+
+      // Lock the popout so it can't be harmed
+      this.locked = true;
+
+      // Animate the popout open and call complete
+      this.$el.stop().animate({ right: 0 }, this.options.animations.speed, this.openCompleted(callback));
+
+    },
+
+    openCompleted : function (callback) {
+
+        // Toggle the open and close classes
+        this.$el.toggleClass(this.options.klasses.open);
+
+        // If a callback has been set, we will execute it
+        if ("function" === typeof callback) {
+          callback();
+        }
+
+        // Unlock the popout so we can go on with life
+        this.locked = false;
+
+    },
+
+    closePopout : function (callback) {
+
+      // Lock the popout so it can't be harmed
+      this.locked = true;
+
+      // Find the width of the popout content
+      var position = -(this.$el.outerWidth() - this.$el.find("." + this.options.klasses.control).outerWidth());
+
+      // Animate the popout close
+      this.$el.stop().animate({ right: position }, this.options.animations.speed, this.closeCompleted(callback));
+
+    },
+
+    closeCompleted : function (callback) {
+
+      // Toggle the open class
+      this.$el.toggleClass(this.options.klasses.open);
+
+      // If a callback has been set, we will execute it
+      if ("function" === typeof callback) {
+        callback();
+      }
+
+      // Unlock the popout so we can go on with life
+      this.locked = false;
+
+    },
+
+    toggleState : function () {
+
+      // If Poppy is open, close it
+      if (window.Poppy.isOpen()) {
+
+        // If the user triggers the popout to close, they're not interested
+        // so let's set the cookie if it hasn't already been set
+        return window.Poppy.closePopout() && window.Poppy.setCookie();
+
+      }
+
+      // Otherwise it's closed so open it, duh
+      else {
+
+        // If the user triggers the popout to open, clearly they're interested
+        // so we will unset the cookie if it has been set
+        return window.Poppy.openPopout() && window.Poppy.unsetCookie();
+
+      }
+
+    },
+
+    isOpen : function () {
+      return this.$el.hasClass(this.options.klasses.open) && ! this.locked;
     },
 
     // Helper method to create a cookie
@@ -184,7 +188,7 @@
 
   // On your mark...get set... GO!
   $(document).ready(function () {
-    window.Popout.init();
+    window.Poppy.init();
   });
 
 }(jQuery, window, window.document));
